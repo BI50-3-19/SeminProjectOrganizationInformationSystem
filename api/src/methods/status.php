20c87591b6a3ad5dd432a451c573c86b61201f7d
rus_anonym <alexandrsemin2033@gmail.com>
2021-12-17 23:02:05 +0300

    add ip to status method

--- api/src/methods/status.php
+++ api/src/methods/status.php
@@ -3,8 +3,18 @@
 require_once __DIR__ . '/../api.php';
 
 $api->addMethod("status", function () {
+    $ip = null;
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
     return (object) array(
         'date' => date("Y-m-d H:i:s"),
-        'uptime' => floatval(@file_get_contents('/proc/uptime'))
+        'uptime' => floatval(@file_get_contents('/proc/uptime')),
+        'ip' => $ip
     );
 });
