b6cb4a829abaef3ce06c83feb716512eea198b32
rus_anonym <alexandrsemin2033@gmail.com>
2021-12-17 23:02:43 +0300

    delete users.login method

--- api/src/index.php
+++ api/src/index.php
@@ -15,7 +15,6 @@ try {
     require_once __DIR__ . '/methods/status.php';
     require_once __DIR__ . '/methods/errors.php';
     require_once __DIR__ . '/methods/users/registration.php';
-    require_once __DIR__ . '/methods/users/login.php';
 
     if (!array_key_exists("method", $_GET)) {
         throw new APIException(2);
