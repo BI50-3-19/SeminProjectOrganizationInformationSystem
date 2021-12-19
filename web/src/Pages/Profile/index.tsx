10a537bfa7dadcd3e66aeb5b486f7c01e3ce2fa9
rus_anonym <alexandrsemin2033@gmail.com>
2021-12-20 00:58:59 +0300

    add exit button to profile

--- web/src/Pages/Profile/index.tsx
+++ web/src/Pages/Profile/index.tsx
@@ -1,77 +1,81 @@
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import {
-  Text,
-  Div,
-  withAdaptivity,
-  Group,
-  Header,
-  SimpleCell,
-  ScreenSpinner,
-  RichCell,
+	Div,
+	withAdaptivity,
+	Group,
+	ScreenSpinner,
+	RichCell,
+	CellButton,
 } from "@vkontakte/vkui";
 
 import API from "../../TS/api";
 
 import AdaptivityLoginPage from "./LoginPage";
-import { Icon28Profile, Icon56UserCircleOutline } from "@vkontakte/icons";
+import { Icon56UserCircleOutline } from "@vkontakte/icons";
+import { Icon28DoorArrowRightOutline } from "@vkontakte/icons";
 import moment from "moment";
 
 const ProfilePage = ({
-  viewWidth,
-  setPopout,
+	viewWidth,
+	setPopout,
 }: {
-  viewWidth: number;
-  setPopout: React.Dispatch<React.SetStateAction<JSX.Element | null>>;
+	viewWidth: number;
+	setPopout: React.Dispatch<React.SetStateAction<JSX.Element | null>>;
 }): JSX.Element => {
-  const [cookies] = useCookies(["token"]);
-  const [sessionToken, setSessionToken] = React.useState<string | null>(null);
+	const [cookies] = useCookies(["token"]);
+	const [sessionToken, setSessionToken] = React.useState<string | null>(null);
 
-  const [profile, setProfileInfo] = React.useState<{
-    id: number;
-    login: string;
-    regDate: Date;
-  } | null>(null);
+	const [profile, setProfileInfo] = React.useState<{
+		id: number;
+		login: string;
+		regDate: Date;
+	} | null>(null);
 
-  useEffect(() => {
-    if (cookies.token) {
-      setPopout(<ScreenSpinner />);
-      const api = new API({ token: cookies.token });
-      api.sessions
-        .info()
-        .then(async () => {
-          setSessionToken(cookies.token);
-          setProfileInfo(await api.users.get());
-        })
-        .catch(() => {
-          setSessionToken(null);
-        })
-        .finally(() => setPopout(null));
-    }
-  }, [cookies.token]);
+	useEffect(() => {
+		if (cookies.token) {
+			setPopout(<ScreenSpinner />);
+			const api = new API({ token: cookies.token });
+			api.sessions
+				.info()
+				.then(async () => {
+					setSessionToken(cookies.token);
+					setProfileInfo(await api.users.get());
+				})
+				.catch(() => {
+					setSessionToken(null);
+				})
+				.finally(() => setPopout(null));
+		}
+	}, [cookies.token, setPopout]);
 
-  if (sessionToken === null) {
-    return (
-      <AdaptivityLoginPage
-        viewWidth={viewWidth}
-        setPopout={setPopout}
-        setSessionToken={setSessionToken}
-      />
-    );
-  }
+	if (sessionToken === null) {
+		return (
+			<AdaptivityLoginPage
+				viewWidth={viewWidth}
+				setPopout={setPopout}
+				setSessionToken={setSessionToken}
+			/>
+		);
+	}
 
-  return (
-    <Div>
-      <Group>
-        <RichCell
-          before={<Icon56UserCircleOutline  />}
-          caption={`Зарегистрирован: ${moment(profile?.regDate).format("DD.MM.YYYY, HH:mm:ss")}`}
-        >
-          Имя пользователя: {profile?.login}
-        </RichCell>
-      </Group>
-    </Div>
-  );
+	return (
+		<Div>
+			<Group mode="plain">
+				<RichCell
+					before={<Icon56UserCircleOutline />}
+					caption={`Зарегистрирован: ${moment(profile?.regDate).format(
+						"DD.MM.YYYY, HH:mm:ss",
+					)}`}
+				>
+					Имя пользователя: {profile?.login}
+				</RichCell>
+				<CellButton before={<Icon28DoorArrowRightOutline />} mode="danger">
+					Выйти из аккаунта
+				</CellButton>
+			</Group>
+		</Div>
+	);
 };
 
 const AdaptivityProfilePage = withAdaptivity(ProfilePage, { viewWidth: true });
