bd7f6cbf08dd4892ba04bd3264c01896e4f39439
rus_anonym <alexandrsemin2033@gmail.com>
2021-12-19 23:50:01 +0300

    fix github pages cors issue

--- web/src/TS/api.ts
+++ web/src/TS/api.ts
@@ -2,145 +2,146 @@ import axios from "axios";
 import moment from "moment";
 
 class APIError extends Error {
-  public code: number;
-  public method: string;
-  public requestParams: API.RequestParams;
+	public code: number;
+	public method: string;
+	public requestParams: API.RequestParams;
 
-  public constructor({
-    message,
-    code,
-    method,
-    requestParams,
-  }: {
-    code: number;
-    message: string;
-    method: string;
-    requestParams: API.RequestParams;
-  }) {
-    super(message);
-    this.name = this.constructor.name;
-    this.code = code;
-    this.method = method;
-    this.requestParams = requestParams;
-    Error.captureStackTrace(this, this.constructor);
-  }
+	public constructor({
+		message,
+		code,
+		method,
+		requestParams,
+	}: {
+		code: number;
+		message: string;
+		method: string;
+		requestParams: API.RequestParams;
+	}) {
+		super(message);
+		this.name = this.constructor.name;
+		this.code = code;
+		this.method = method;
+		this.requestParams = requestParams;
+		Error.captureStackTrace(this, this.constructor);
+	}
 
-  public get [Symbol.toStringTag](): string {
-    return this.constructor.name;
-  }
+	public get [Symbol.toStringTag](): string {
+		return this.constructor.name;
+	}
 
-  public toJSON(): Pick<this, keyof this> {
-    const json = {} as Pick<this, keyof this>;
+	public toJSON(): Pick<this, keyof this> {
+		const json = {} as Pick<this, keyof this>;
 
-    for (const key of Object.getOwnPropertyNames(this)) {
-      json[key as keyof this] = this[key as keyof this];
-    }
+		for (const key of Object.getOwnPropertyNames(this)) {
+			json[key as keyof this] = this[key as keyof this];
+		}
 
-    return json;
-  }
+		return json;
+	}
 }
 
 class UsersAPI {
-  constructor(private api: API) {}
+	constructor(private api: API) {}
 
-  public async registration({
-    login,
-    password,
-  }: API.Users.RegistrationParams): Promise<API.SuccessResponse> {
-    return this.api.call("users.registration", { login, password });
-  }
+	public async registration({
+		login,
+		password,
+	}: API.Users.RegistrationParams): Promise<API.SuccessResponse> {
+		return this.api.call("users.registration", { login, password });
+	}
 
-  public async get({
-    token,
-  }: API.RequiredToken  = {}): Promise<API.Users.GetResponse> {
-    const response = await this.api.call("users.get", {
-      token: token || this.api.token || "",
-    });
+	public async get({
+		token,
+	}: API.RequiredToken = {}): Promise<API.Users.GetResponse> {
+		const response = await this.api.call("users.get", {
+			token: token || this.api.token || "",
+		});
 
-    return {
-      ...response,
-      id: Number(response.id),
-      regDate: moment(response.regDate, "YYYY-MM-DD HH:mm:ss").toDate(),
-    };
-  }
+		return {
+			...response,
+			id: Number(response.id),
+			regDate: moment(response.regDate, "YYYY-MM-DD HH:mm:ss").toDate(),
+		};
+	}
 }
 
 class SessionsAPI {
-  constructor(private api: API) {}
+	constructor(private api: API) {}
 
-  public async create({
-    login,
-    password,
-  }: API.Sessions.CreateParams): Promise<API.Sessions.CreateResponse> {
-    const response = await this.api.call("sessions.create", {
-      login,
-      password,
-    });
+	public async create({
+		login,
+		password,
+	}: API.Sessions.CreateParams): Promise<API.Sessions.CreateResponse> {
+		const response = await this.api.call("sessions.create", {
+			login,
+			password,
+		});
 
-    return {
-      ...response,
-      created: moment(response.created, "YYYY-MM-DD HH:mm:ss").toDate(),
-    };
-  }
+		return {
+			...response,
+			created: moment(response.created, "YYYY-MM-DD HH:mm:ss").toDate(),
+		};
+	}
 
-  public async info({
-    token,
-  }: API.RequiredToken = {}): Promise<API.Sessions.InfoResponse> {
-    const response = await this.api.call("sessions.info", {
-      token: token || this.api.token || "",
-    });
+	public async info({
+		token,
+	}: API.RequiredToken = {}): Promise<API.Sessions.InfoResponse> {
+		const response = await this.api.call("sessions.info", {
+			token: token || this.api.token || "",
+		});
 
-    return {
-      ...response,
-      id: Number(response.id),
-      created: moment(response.created, "YYYY-MM-DD HH:mm:ss").toDate(),
-    };
-  }
+		return {
+			...response,
+			id: Number(response.id),
+			created: moment(response.created, "YYYY-MM-DD HH:mm:ss").toDate(),
+		};
+	}
 
-  public async reset({
-    token,
-  }: API.RequiredToken): Promise<API.SuccessResponse> {
-    return this.api.call("sessions.reset", {
-      token: token || this.api.token || "",
-    });
-  }
+	public async reset({
+		token,
+	}: API.RequiredToken): Promise<API.SuccessResponse> {
+		return this.api.call("sessions.reset", {
+			token: token || this.api.token || "",
+		});
+	}
 }
 
 class API {
-  private apiUrl = "http://176.113.82.100/index.php";
-  public token?: string;
-  constructor({ apiUrl, token }: { apiUrl?: string; token?: string } = {}) {
-    if (apiUrl) {
-      this.apiUrl = apiUrl;
-    }
-    if (token) {
-      this.token = token;
-    }
-  }
+	private apiUrl =
+		"https://cors-anywhere.herokuapp.com/176.113.82.100/index.php";
+	public token?: string;
+	constructor({ apiUrl, token }: { apiUrl?: string; token?: string } = {}) {
+		if (apiUrl) {
+			this.apiUrl = apiUrl;
+		}
+		if (token) {
+			this.token = token;
+		}
+	}
 
-  public users = new UsersAPI(this);
-  public sessions = new SessionsAPI(this);
+	public users = new UsersAPI(this);
+	public sessions = new SessionsAPI(this);
 
-  public async call(method: string, params: Record<string, string> = {}) {
-    const response = await axios.get(this.apiUrl, {
-      params: {
-        ...params,
-        method,
-      },
-    });
+	public async call(method: string, params: Record<string, string> = {}) {
+		const response = await axios.get(this.apiUrl, {
+			params: {
+				...params,
+				method,
+			},
+		});
 
-    if (response.data.error) {
-      const error = response.data.error;
-      throw new APIError({
-        code: error.code,
-        message: error.message,
-        method,
-        requestParams: error.requestParams,
-      });
-    } else {
-      return response.data.response;
-    }
-  }
+		if (response.data.error) {
+			const error = response.data.error;
+			throw new APIError({
+				code: error.code,
+				message: error.message,
+				method,
+				requestParams: error.requestParams,
+			});
+		} else {
+			return response.data.response;
+		}
+	}
 }
 
 export { APIError };
